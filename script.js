d0e68a64e35956f28db71ba1e4b268b8714d090a
eslam33333333 <163295581+eslam33333333@users.noreply.github.com>
2025-08-26 12:39:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// اغلاق القائمة بعد الضغط على أي لينك
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => {
-    document.getElementById('menu-toggler').checked = false;
+
+// نختار عناصر الـ nav
+const menuToggler = document.getElementById("menu-toggler");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+// لما يضغط على أي لينك → شيل العلامة من الـ checkbox
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menuToggler.checked = false;
   });
 });
