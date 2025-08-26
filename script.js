a85c141fb10e847da23b9c870fb182f6b418510e
eslam33333333 <163295581+eslam33333333@users.noreply.github.com>
2025-08-26 12:25:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
-
-
-
+// اغلاق القائمة بعد الضغط على أي لينك
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById('menu-toggler').checked = false;
+  });
+});
