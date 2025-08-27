d8646d81969861166ba3f63715e5009c0c34f3fe
eslam33333333 <163295581+eslam33333333@users.noreply.github.com>
2025-08-27 08:06:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-// نختار عناصر الـ nav
-const menuToggler = document.getElementById("menu-toggler");
-const navLinks = document.querySelectorAll(".nav-links a");
-
-// لما يضغط على أي لينك → شيل العلامة من الـ checkbox
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    menuToggler.checked = false;
-  });
-});
-
-
-
 
 
 
